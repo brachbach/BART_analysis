7e406a419d235c31ffd04f54a6f4b5df19cd055c
Ben Rachbach <brachbach@gmail.com>
2019-08-29 23:45:00 -0700

    Label axis ticks, switch to properly using data from weekday, code cleanup

--- one_time_processing/save_schedule.py
+++ one_time_processing/save_schedule.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 public_api_key = 'MW9S-E7SL-26DU-VV8V'
 
-schedule = requests.get('https://api.bart.gov/api/sched.aspx?cmd=stnsched&orig=embr&key=MW9S-E7SL-26DU-VV8V&date=08/11/2019&json=y')
+schedule = requests.get('https://api.bart.gov/api/sched.aspx?cmd=stnsched&orig=embr&key=MW9S-E7SL-26DU-VV8V&date=08/13/2019&json=y')
 
 schedule_df = pd.DataFrame(schedule.json()['root']['station']['item'])
 
-schedule_df.to_csv(path_or_buf="processed_data/08-11-2019-embr-sched.csv")
+schedule_df.to_csv(path_or_buf="processed_data/08-13-2019-embr-sched.csv")
