75a48e9eead2c7345f17c41bdf4bda05d87fcd8f
Ben Rachbach <brachbach@gmail.com>
2019-08-29 21:59:31 -0700

    Grab Embarcadero schedule

---
+++ one_time_processing/save_schedule.py
@@ -0,0 +1,10 @@
+import requests
+import pandas as pd
+
+public_api_key = 'MW9S-E7SL-26DU-VV8V'
+
+schedule = requests.get('https://api.bart.gov/api/sched.aspx?cmd=stnsched&orig=embr&key=MW9S-E7SL-26DU-VV8V&date=08/11/2019&json=y')
+
+schedule_df = pd.DataFrame(schedule.json()['root']['station']['item'])
+
+schedule_df.to_csv(path_or_buf="processed_data/08-11-2019-embr-sched.csv")
